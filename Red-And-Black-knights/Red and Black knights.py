5aa5e134ee7fd1084897da38e8fccc9456b66d8c
cthutulu <cthutulu@gmail.com>
2026-08-11 11:46:54 +0200

    Black And Red knights: intro and tasks

---
+++ Red-And-Black-knights/Red and Black knights.py
@@ -0,0 +1,33 @@
+"""
+- Bræt
+    * spiral fra 0 til x
+    * med x = højeste tal og derfor størrelsen på brættet
+
+- Brikker
+    * forskellige klasser
+        * kan flytte sig på forskellige måder
+        * forskellige farver
+    * en måde at sige hvilke brikker der skal bruges
+    * en måde at sige hvor mange  forskellige farver der skal være
+
+- placering og regler
+    * første brik skal placeres på 0
+    * brikker skal placeres på det mindste felt, der ikke er optaget eller kan "angribes"
+    * skifte mellem alle aktive brikker
+    * brikker kan "angribe" tomme felter
+
+- resultat
+    * danne et resultat man kan se
+    * skal kunne passe til skærmen lige meget størrelsen
+    * zoome ind og ud
+
+
+rows and columns
+
+(x + 1) * 2
+
+
+
+
+"""
+
